0fdcf02bef2c259bd9bf8b084ac2f34c1323fbf8
Raghu Nandan Chilukuri <raghu.rnc@gmail.com>
2015-10-07 20:14:20 +0530

    Capture user inputs

--- examples/demo/new_page.jl
+++ examples/demo/new_page.jl
@@ -16,15 +16,8 @@ function show_movie_input(m, updates, idx)
        size(120px, 180px) |> vbox |> packacross(center)
     desc = vbox( fontsize(1em, m), getfield(m, "Genre")    )
 
-    #rating_widget = radiogroup([radio("0", "0"),radio("1", "1"), radio("2", "2"),radio("3", "3"),radio("4", "4"),radio("5", "5")];
-    #                          name="Your Rating (0 for \"didn't watch\" )"
-    #                         )
-
     rating_slider = slider(0:5; name="rating-$idx", value=0, editable=true, pin=true, disabled=false, secondaryprogress=0)
     rating_widget = addinterpreter(r -> (idx, r), rating_slider) >>> updates
-
-    #user_ratingᵗ = lift(user_rating) do
-    #end
 
     content = vbox(poster,vskip(0.7em),
          getfield(m, "Title", m) |> fontsize(1.2em), vskip(0.5em),
@@ -34,21 +27,9 @@ function show_movie_input(m, updates, idx)
 
 end
 
-#=
-function updaterating!(user_rating)
-end
-
-function ratingwidget(movietile, movielist)
-
-    user_rating = Input(0)
-    rating_widget = subscribe(slider(0:5; name="Your rating", value=0, editable=true, pin=true, disabled=false, secondaryprogress=0),
-                              user_rating )
-    user_ratingᵗ = lift(user_rating) do
-    end
-
-    vbox(movietile, vskip(1em), rating_widget)
-end
-=#
+#function createratingvec(movielist, submitted_ratings)
+	
+#end
 
 
 function main(window)
@@ -59,7 +40,6 @@ function main(window)
     s = sampler()
 
     username = textinput("";name=:username, label="Your Name Here", floatinglabel=false, maxlength=256)
-    #submit_button = button(      map(pad([left, right], 1em), ["Submit", "Now"]) ; name=:submit, raised=true, disabled=false, noink=true )
     btn = Input(leftbutton)
     submit_button = button("Submit") >>> btn
 
@@ -68,7 +48,7 @@ function main(window)
 ### GET A LIST OF MOVIES, num_movies IS THE NUMBER OF MOVIES YOU WANT ###
 ## nrows is the number of rows in the display, and ncols is the number of columns per row.
 
-    n = 12
+    n = 20
     movielist = movie_dataset[floor(rand(n)*1600), :]
     init_ratings = zeros(Int, n)
     input = Input((0, 0))
@@ -77,12 +57,23 @@ function main(window)
         state
     end
     submitted_ratings = sampleon(btn, ratings)
-    #ratingvec = zeros(size(movie_dataset))
+    clicked = Input(0)
+    #if(bool(value(sampleon(btn, clicked))))
+        #push!(clicked, 1)
+    #    lift(x -> 1, clicked)
+    #end
 
     list = hbox([show_movie_input(m, input, idx) for (idx, m) in enumerate(movielist[:,2])]) |> wrap
 
+    ratingvec = spzeros(1, size(movie_dataset)[1])
+    map([1:size(movielist)[1]]) do movnum
+        #ratingvec[int(movielist[movnum, 1])] = value(ratings))[movnum]
+        println(value(submitted_ratings)[movnum])
+    end 
+    
+    println(nonzeros(ratingvec))
 
-    vbox(vlist0, ratings, submitted_ratings, vskip(3em),width(10em, submit_button), vskip(3em), list ) |> Escher.pad(2em)
+    vbox(vlist0, ratings, submitted_ratings, vskip(3em),clicked, vskip(3em), width(10em, submit_button), vskip(3em), list, vskip(3em), [int(movielist[:, 1])], nonzeros(ratingvec) ) |> Escher.pad(2em)
 
 end
 
